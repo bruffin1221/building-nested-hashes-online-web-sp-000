497d43118aa4460767175b4364c7906cf4bf4705
Bruce Ruffin <bruffin1221@gmail.com>
2019-09-04 03:10:14 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,17 +1,22 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => []
+   },
+   :capulet => {
+   :patriarch => {name: "Lord Capulet", age: "50"},
+   :matriarch => {name: "Lady Capulet", age: "51"},
+   :heroine => {name: "Juliet", age: "15", status: "alive"},
+   :heroine_friends => []  },
         },
       :hero => {
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -26,5 +31,5 @@ def third_challenge
    }
   }
 
-  
+
 end
