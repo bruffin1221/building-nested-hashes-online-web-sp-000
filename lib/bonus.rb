15d04402c855cfd421f1a0f8ae676ac4fcc0a916
Bruce Ruffin <bruffin1221@gmail.com>
2019-09-04 03:47:47 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,11 +19,11 @@ def bonus
       ]
    }
   }
-
+end
   #code your solution here:
 epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
-
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-puts  epic_tragedy
+puts epic_tragedy
 end
+  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+ epic_tragedy
