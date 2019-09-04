b6d5c9cd9c62f991f62a95bc7e15852d2fbdb420
Bruce Ruffin <bruffin1221@gmail.com>
2019-09-04 03:20:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,26 +10,7 @@ def third_challenge
    :patriarch => {name: "Lord Capulet", age: "50"},
    :matriarch => {name: "Lady Capulet", age: "51"},
    :heroine => {name: "Juliet", age: "15", status: "alive"},
-   :heroine_friends => []  },
-        },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
-   },
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
+   :heroine_friends => []
   }
-
-
+  }
 end
